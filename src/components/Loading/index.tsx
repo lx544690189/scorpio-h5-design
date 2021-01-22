3e885552f9ec1d4e65c91f0e252ad41dca147a04
lixin <lixin544@163.com>
2021-01-22 16:50:43 +0800

    添加loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.less';
+
+export default function() {
+  return (
+    <figure>
+      <div className="dot white"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </figure>
+  );
+}
