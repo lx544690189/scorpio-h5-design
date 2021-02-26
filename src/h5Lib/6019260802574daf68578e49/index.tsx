d3a147c6991982a704e2bfd87a5608e0b6ea69a5
lixin <lixin544@163.com>
2021-02-26 17:23:28 +0800

    测试

--- src/h5Lib/6019260802574daf68578e49/index.tsx
+++ src/h5Lib/6019260802574daf68578e49/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import QierPlayer from 'qier-player';
+// import QierPlayer from 'qier-player';
 import style from './index.less';
 import { pxToVw } from '@/utils';
+import { Button } from 'antd';
 
 export default function(props:any) {
   const {srcOrigin, width, height, themeColor} = props;
   return (
     <div className={style.videoContainer}>
-      <QierPlayer
+      <Button />
+      {/* <QierPlayer
         key={`${width}-${height}`}
         width={`${pxToVw(width)}vw`}
         height={`${pxToVw(height)}vw`}
@@ -15,7 +17,7 @@ export default function(props:any) {
         showVideoQuality={false}
         themeColor={themeColor}
         srcOrigin={srcOrigin}
-      />
+      /> */}
     </div>
   );
 }
---
+++ src/h5Lib/index.ts
@@ -0,0 +1,5 @@
+export default {
+  '600fd4f8607ea614fc3c894c': import('../h5Lib/600fd4f8607ea614fc3c894c'),
+  '601912a902574daf68578e48': import('../h5Lib/601912a902574daf68578e48'),
+  '6019260802574daf68578e49': import('../h5Lib/6019260802574daf68578e49'),
+};
