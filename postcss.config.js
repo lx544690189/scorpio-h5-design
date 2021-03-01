d3257d5801472da5ff0e4024ea430fb598055cef
lixin <lixin544@163.com>
2021-03-01 17:17:09 +0800

    add postcss-px-to-viewport

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   modules: true,
+  plugins: {
+    'postcss-px-to-viewport-fixed': {
+      viewportWidth: 750, // (Number) The width of the viewport.
+      viewportHeight: 1334, // (Number) The height of the viewport.
+      unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
+      viewportUnit: 'vw', // (String) Expected units.
+      selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
+      minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
+      mediaQuery: false, // (Boolean) Allow px to be converted in media queries.
+      include: /h5Lib/i,
+      exclude: /node_modules/i,
+    },
+  },
 };
