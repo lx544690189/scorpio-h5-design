885cd717b904ee9cd7e282f59bea3016f3f7275f
lixin <lixin544@163.com>
2021-03-02 18:52:55 +0800

    temp

--- src/pages/mobile/components/DynamicComponent/index.tsx
+++ src/pages/mobile/components/DynamicComponent/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { pxToVw } from '@/utils';
 import React from 'react';
+import h5Lib from '@/h5Lib';
+import Loadable from 'react-loadable';
 
 interface Iprops {
   id: string;
@@ -14,9 +16,27 @@ function computedBorder({top, right, bottom, left}:any){
   return `${pxToVw(top)}vw ${pxToVw(right)}vw ${pxToVw(bottom)}vw ${pxToVw(left)}vw`;
 }
 
+function Loading(props:any) {
+  if (props.isLoading) {
+    if (props.timedOut) {
+      return <div>Loader timed out!</div>;
+    } else if (props.pastDelay) {
+      return <div>Loading...</div>;
+    } else {
+      return null;
+    }
+  } else if (props.error) {
+    return <div>Error! Component failed to load</div>;
+  } else {
+    return null;
+  }
+}
+
 export default function(props: Iprops) {
   const { id, uuid, isSelected, componentProps, containerProps } = props;
-  const Component = require(`@/h5Lib/${id}/index.tsx`).default;
+  // const Component = require(`@/h5Lib/${id}/index.tsx`).default;
+  // @ts-expect-error
+  const Component = h5Lib[id];
   const {
     color, fontSize, fontWeight,
     backgroundImage, backgroundColor, backgroundSize, backgroundRepeat,
@@ -40,10 +60,14 @@ export default function(props: Iprops) {
     margin: margin ? computedBorder(margin): 0,
     padding: padding ? computedBorder(padding): 0,
   };
+  const LoadableComponent = Loadable({
+    loader: ()=>Component,
+    loading: Loading,
+  });
 
   return (
     <div data-uuid={uuid} data-selected={isSelected} style={style}>
-      <Component {...componentProps}/>
+      <LoadableComponent {...componentProps}/>
     </div>
   );
 }
