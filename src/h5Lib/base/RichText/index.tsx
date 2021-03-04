3c6f3a16a1240c9cf20fd9c0ae7231bf1cc3278f
lixin <lixin544@163.com>
2021-03-04 22:13:07 +0800

    css

--- src/h5Lib/base/RichText/index.tsx
+++ src/h5Lib/base/RichText/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './index.less';
+import style from './index.less';
 import 'braft-editor/dist/output.css';
 
 export default function(props:any) {
   return (
-    <div className="braft-output-content" dangerouslySetInnerHTML={{__html: props.html}}></div>
+    <div className={style.braftOutputContent} dangerouslySetInnerHTML={{__html: props.html}}></div>
   );
 }
