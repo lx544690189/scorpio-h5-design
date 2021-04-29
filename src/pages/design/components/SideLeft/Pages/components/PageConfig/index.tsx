0efcb1d66ef48bb80c5ab3e7a3bf89a9568861dc
lixin <lixin544@163.com>
2021-04-29 16:29:16 +0800

    fix: 移除formily(新版本不兼容导致项目报错)

--- src/pages/design/components/SideLeft/Pages/components/PageConfig/index.tsx
+++ src/pages/design/components/SideLeft/Pages/components/PageConfig/index.tsx
@@ -1,56 +1,56 @@
 import React, { useEffect } from 'react';
-import { Drawer, Button  } from 'antd';
+import { Drawer, Button } from 'antd';
 import './index.less';
 import 'antd/dist/antd.css';
+import { useForm } from 'form-render';
 import { useModel } from 'umi';
-import {
-  Form,
-  FormItem,
-  createAsyncFormActions,
-} from '@formily/antd';
-import {
-  Input,
-} from '@formily/antd-components'; // 或者@formily/next-components
 import { v4 as uuidv4 } from 'uuid';
 import { page_generatorSchema } from '@/constant';
+import FormRenderWithWidgets from '@/components/FormRenderWithWidgets';
 
-const actions = createAsyncFormActions();
-const pageNumber = 1;
+const schema = {
+  'type': 'object',
+  'properties': {
+    'title': {
+      'title': '页面标题',
+      'type': 'string',
+      'required': true,
+    },
+    'path': {
+      'title': '路径',
+      'type': 'string',
+      'required': true,
+    },
+  },
+};
 
 export default function() {
   const { pageSchema } = useModel('bridge');
   const { page, closeConfigPageDrawer, onCreatePage } = useModel('page');
   useEffect(() => {
-    if(page.configModalVisible){
-      actions.setFieldState('title', (state) => {
-        state.value = `页面-${pageSchema.length + 1}`;
-      });
-      actions.setFieldState('path', (state) => {
-        state.value = uuidv4();
+    if (page.configModalVisible) {
+      form.setValues({
+        title: `页面-${pageSchema.length + 1}`,
+        path: uuidv4(),
       });
     }
   }, [page.configModalVisible]);
+  const form = useForm();
 
-  const submit1 = async() => {
-    const validate = await actions.submit();
-    const {title, path} = validate.values;
-    onCreatePage({
-      config: {
-        title,
-        path,
-      },
-      generatorSchema: page_generatorSchema,
-      props: {
-        title,
-        path,
-        isHome: true,
-        backgroundColor: '#fff',
-      },
-      components: [],
-    });
-    // actions.submit();
+  const onFinish = function(formData: Record<string, unknown>, errors:any[]) {
+    if (errors.length === 0) {
+      onCreatePage({
+        config: formData,
+        generatorSchema: page_generatorSchema,
+        props: {
+          ...formData,
+          isHome: true,
+          backgroundColor: '#fff',
+        },
+        components: [],
+      });
+    }
   };
-
 
   return (
     <Drawer
@@ -71,28 +71,17 @@ export default function() {
           <Button onClick={closeConfigPageDrawer} style={{ marginRight: 8 }}>
             取消
           </Button>
-          <Button onClick={submit1} type="primary">
+          <Button onClick={form.submit} type="primary">
             确定
           </Button>
         </div>
       }
     >
-      <Form actions={actions}>
-        <FormItem
-          label="页面标题"
-          name="title"
-          component={Input}
-          labelCol={{ span: 6 }}
-          required
-        />
-        <FormItem
-          label="路径"
-          name="path"
-          component={Input}
-          labelCol={{ span: 6 }}
-          required
-        />
-      </Form>
+      <FormRenderWithWidgets
+        form={form}
+        schema={schema}
+        onFinish={onFinish}
+      />
     </Drawer>
   );
 }
