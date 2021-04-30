64147dc6ea115c8f797d635fa54f2a688cb49021
lixin <lixin544@163.com>
2021-04-30 10:54:44 +0800

    移除无用文件

--- src/models/componentEdit.ts
+++
@@ -1,9 +0,0 @@
-import { useState, useCallback } from 'react';
-export default function componentEdit() {
-  const [pageSchema, setPageSchema] = useState([]);
-
-  return {
-    pageSchema,
-    setPageSchema,
-  };
-}
